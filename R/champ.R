27fb042f791dd46604c62ddbde4ebca1ac6d94f8
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-09-13 23:02:01 -0500

    Update champ.R

--- R/champ.R
+++ R/champ.R
@@ -1,3 +1,13 @@
+#####################################################################
+###   Title : Genome-wide DNA methylation analysis for atrial fibrillation patient
+###   Author: Shicheng Guo, Ph.D. Email: Shihcheng.Guo@Gmail.com 
+###   Section 1. Function predifinition 
+###   Section 2. Data Cleaning
+###   Section 3. Differential Analysis
+###   Section 4. Pathway Analysis
+###   Section 5. GEO Validation (GSE34639,GSE27895)
+#####################################################################
+
 BiocManager::install("ChAMP") 
 BiocManager::install("doParallel") 
 
