6d707f5a73f957141cac7620b8d57c8c90b77b8e
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-09-16 18:15:00 -0500

    Update minfi.R

--- R/minfi.R
+++ R/minfi.R
@@ -35,9 +35,15 @@ phen  <- pData(myNormalRGSet)$Sample_Group
 anno<-annotation(myNormalRGSet)
 
 predictedSex <- getSex(myNormalRGSet, cutoff = -2)$predictedSex
+anno<-read.table("~/hpc/db/GPL21145-48548_EPIC.txt",head=T,sep="\t")
 
 dmp <- dmpFinder(beta, pheno = phen  , type = "categorical")
-write.table(dmp,file="dmp.dmpFinder.minfi.txt",sep="\t",col.names=NA,row.names=T,quote=F)
+dmp.full<-data.frame(dmp,anno[match(rownames(dmp),anno$ID),])
+write.table(dmp.full,file="dmp.dmpFinder.minfi.txt",sep="\t",col.names=NA,row.names=T,quote=F)
+
+dmp.sig<-subset(dmp,pval<5.7^-8)
+dmp.sig<-data.frame(dmp.sig,anno[match(rownames(dmp.sig),anno$ID),c(1,12,13,7)])
+
 designMatrix <- model.matrix(~ phen)
 dmr <- bumphunter(myNormalRGSet, design = designMatrix, cutoff = 0.2, B=0, type="Beta")
 write.table(dmr$table,file="dmr.bumphunter.minfi.txt",sep="\t",col.names=NA,row.names=T,quote=F)
@@ -46,11 +52,6 @@ cellCounts <- estimateCellCounts(RGSet)
 write.table(cellCounts,file="cellcounts.estimation.txt",sep="\t",col.names=NA,row.names=T,quote=F)
 
 anno<-read.table("~/hpc/db/GPL21145-48548_EPIC.txt",head=T,sep="\t")
-newdmp<-data.frame(dmp,anno[match(rownames(dmp),anno$ID),])
 
 wget https://raw.githubusercontent.com/Shicheng-Guo/GscRbasement/master/manhattan.qqplot.minfi.R -O manhattan.qqplot.minfi.R
 Rscript manhattan.qqplot.minfi.R dmp.dmpFinder.minfi.txt
-
-
-
-
