99ea492c356fb777cfb8c59b850bbc3896b7e7a7
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-09-16 15:43:13 -0500

    Update champ.R

--- R/champ.R
+++ R/champ.R
@@ -7,7 +7,6 @@
 ###   Section 4. Pathway Analysis
 ###   Section 5. GEO Validation (GSE34639,GSE27895)
 #########################################################################################
-
 BiocManager::install("ChAMP") 
 BiocManager::install("doParallel") 
 BiocManager::install("benchmarkme") 
@@ -18,23 +17,29 @@ library("ChAMP")
 library("doParallel")
 Dir="/home/local/MFLDCLIN/guosa/hpc/methylation/Ingrid/MCaldwell-Sept27-17-HuMethEPIC/Raw_Data/idat"
 
+set.seed(11)
+
 myLoad <- champ.load(Dir,filterBeads=TRUE,arraytype="EPIC")
 pdf("MCaldwell.AMP.EPIC.QC.pdf")
 champ.QC()
 dev.off()
 
 ##########################################################################
-myNorm <- champ.norm(beta=myLoad$beta,arraytype="EPIC",cores=6)
 pdf("MCaldwell.AMP.EPIC.SVD.pdf")
 champ.SVD(beta=myNorm,pd=myLoad$pd)
 dev.off()
 myCombat <- champ.runCombat(beta=myNorm,pd=myLoad$pd,batchname=c("Slide"))
 ##########################################################################
-
-myDMP <- champ.DMP(beta = myNorm,pheno=myLoad$pd$pureG3,arraytype="EPIC")
+# don't use all the cores which will easily be killed by system
+detectCores()
+seed=sample(seq(1,10000,by=1),1)
+myNorm <- champ.norm(beta=myLoad$beta,arraytype="EPIC",cores=5)
+myDMP <- champ.DMP(beta = myNorm,pheno=myLoad$pd$pureG3,compare.group=c("Case","Control"),arraytype="EPIC")
+write.table(myDMP,file=paste("AtrialFibrillation.",seed,".24Case4Control.myDMP.txt",sep=""),col.names = NA,row.names = T,quote=F,sep="\t")
 
 myDMP <- champ.DMP(beta = myNorm,pheno=myLoad$pd$Sample_Group,arraytype="EPIC")
-write.table(myDMP,file="AtrialFibrillation.CaseControl.myDMP.txt",col.names = NA,row.names = T,quote=F,sep="\t")
+write.table(myDMP,file=paste("AtrialFibrillation.",seed,".CaseControl.myDMP.txt",sep=""),col.names = NA,row.names = T,quote=F,sep="\t")
+
 myDMP <- champ.DMP(beta = myNorm,pheno=myLoad$pd$Young_Old,arraytype="EPIC",adjPVal = 0.1)
 write.table(myDMP,file="AtrialFibrillation.YoungOld.myDMP.txt",col.names = NA,row.names = T,quote=F,sep="\t")
 myDMP <- champ.DMP(beta = myNorm,pheno=myLoad$pd$Pmr_Enrollment,arraytype="EPIC",adjPVal = 0.1)
